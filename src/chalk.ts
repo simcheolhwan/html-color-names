f9c5f9c8a9da4200afba73480d36d264fb2e334e
sim <sim@cheolhwan.com>
2023-02-22 19:38:49 +0900

    Log chalk colors on console

---
+++ src/chalk.ts
@@ -0,0 +1,25 @@
+import chalk from "chalk"
+import text from "./text"
+
+const colors = {
+  black: chalk.black,
+  red: chalk.red,
+  green: chalk.green,
+  yellow: chalk.yellow,
+  blue: chalk.blue,
+  magenta: chalk.magenta,
+  cyan: chalk.cyan,
+  white: chalk.white,
+  gray: chalk.gray,
+  redBright: chalk.redBright,
+  greenBright: chalk.greenBright,
+  yellowBright: chalk.yellowBright,
+  blueBright: chalk.blueBright,
+  magentaBright: chalk.magentaBright,
+  cyanBright: chalk.cyanBright,
+  whiteBright: chalk.whiteBright,
+}
+
+for (const [name, color] of Object.entries(colors)) {
+  console.log(color(name.padEnd(16, " ") + text))
+}
